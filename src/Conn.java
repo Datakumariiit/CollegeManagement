4b2f9f4ba75b91308552355b3320c89c591707ec
DATA KUMAR <bt21cse084@iiitn.ac.in>
2023-11-11 21:43:19 +0530

    first_commit

---
+++ src/Conn.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/universitymanagementsystem", "root", "iiitn");
+            s = c.createStatement();
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
